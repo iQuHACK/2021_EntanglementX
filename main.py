a38dd4802275aad4f3f06ac491f5fefb6d248379
mohfaz <mobayen.mohammad@gmail.com>
2021-01-30 19:31:40 -0500

    The initial gui is created

---
+++ main.py
@@ -0,0 +1,102 @@
+from tkinter import *
+
+class Cell():
+    FILLED_COLOR_BG = "green"
+    EMPTY_COLOR_BG = "white"
+    FILLED_COLOR_BORDER = "green"
+    EMPTY_COLOR_BORDER = "black"
+
+    def __init__(self, master, x, y, size):
+        """ Constructor of the object called by Cell(...) """
+        self.master = master
+        self.abs = x
+        self.ord = y
+        self.size= size
+        self.fill= False
+
+    def _switch(self):
+        """ Switch if the cell is filled or not. """
+        self.fill= not self.fill
+        
+    def draw(self):
+        """ order to the cell to draw its representation on the canvas """
+        if self.master != None :
+            fill = Cell.FILLED_COLOR_BG
+            outline = Cell.FILLED_COLOR_BORDER
+
+            if not self.fill:
+                fill = Cell.EMPTY_COLOR_BG
+                outline = Cell.EMPTY_COLOR_BORDER
+
+            xmin = self.abs * self.size
+            xmax = xmin + self.size
+            ymin = self.ord * self.size
+            ymax = ymin + self.size
+
+            self.master.create_rectangle(xmin, ymin, xmax, ymax, fill = fill, outline = outline)
+
+class CellGrid(Canvas):
+    def __init__(self,master, rowNumber, columnNumber, cellSize, *args, **kwargs):
+        Canvas.__init__(self, master, width = cellSize * columnNumber , height = cellSize * rowNumber, *args, **kwargs)
+
+        self.cellSize = cellSize
+
+        self.grid = []
+        for row in range(rowNumber):
+
+            line = []
+            for column in range(columnNumber):
+                line.append(Cell(self, column, row, cellSize))
+
+            self.grid.append(line)
+
+        #memorize the cells that have been modified to avoid many switching of state during mouse motion.
+        self.switched = []
+
+        #bind click action
+        self.bind("<Button-1>", self.handleMouseClick)  
+        #bind moving while clicking
+        self.bind("<B1-Motion>", self.handleMouseMotion)
+        #bind release button action - clear the memory of midified cells.
+        self.bind("<ButtonRelease-1>", lambda event: self.switched.clear())
+
+        self.draw()
+
+
+
+    def draw(self):
+        for row in self.grid:
+            for cell in row:
+                cell.draw()
+
+    def _eventCoords(self, event):
+        row = int(event.y / self.cellSize)
+        column = int(event.x / self.cellSize)
+        return row, column
+
+    def handleMouseClick(self, event):
+        row, column = self._eventCoords(event)
+        pr
+        cell = self.grid[row][column]
+        cell._switch()
+        cell.draw()
+        #add the cell to the list of cell switched during the click
+        self.switched.append(cell)
+
+    def handleMouseMotion(self, event):
+        row, column = self._eventCoords(event)
+        cell = self.grid[row][column]
+
+        if cell not in self.switched:
+            cell._switch()
+            cell.draw()
+            self.switched.append(cell)
+
+
+if __name__ == "__main__" :
+    app = Tk()
+    num = int(input ("Enter number :") )
+    grid = CellGrid(app, num, num, 100)
+    grid.pack()
+
+    app.mainloop()
