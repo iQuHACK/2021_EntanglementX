9c05102a30e902e71200ea6dba986e3463f5abcf
Matheus Andrade <matheusgandrade@poli.ufrj.br>
2021-01-31 10:31:02 -0300

    coin operator and simplification for flip flop

--- qwalk_circuit.py
+++ qwalk_circuit.py
@@ -11,18 +11,20 @@ class QwalkerGridCircuit():
         self.graph = graph
         self.dynamic = dynamic
 
-        self.vreg_len, self.ereg_len = self.num_qubits()
-        self.reg_len = self.vreg_len + self.ereg_len
-
         self.N = int(np.ceil(np.log2(dim1)))
         self.M = int(np.ceil(np.log2(dim2)))
+
+        self.vreg_len = self.N + self.M
+        self.ereg_len = 2
+        self.reg_len = self.vreg_len + self.ereg_len
+
 
         self.circuit = QuantumCircuit(self.reg_len, self.reg_len)
 
 
-    def num_qubits(self):
-        return [int(x) for x in np.ceil( 
-            np.log2([self.graph.number_of_nodes(), 4]))]
+    # def num_qubits(self):
+    #     return [int(x) for x in np.ceil( 
+    #         np.log2([self.graph.number_of_nodes(), 4]))]
 
 
     def flip_flop(self):
@@ -93,7 +95,7 @@ class QwalkerGridCircuit():
         return circuit
 
 
-    def coin_operator(self, vertex_coins=None):
+    def coin(self, vertex_coins=None):
 
         circuit = QuantumCircuit(self.reg_len)
         if vertex_coins is None:
@@ -102,12 +104,77 @@ class QwalkerGridCircuit():
             circuit.barrier()
 
         else:
-            for vertex, coin in vertex_coins:
-                circuit.append(
-                    coin.control(self.vreg_len, label="C", ctrl_state=vertex))
+            for vertex in vertex_coins:
+
+                vcoin = QuantumCircuit(self.ereg_len, name='HH')
+                vcoin.x(range(self.ereg_len))
+                cvcoin = vcoin.to_gate().control(
+                    num_ctrl_qubits=self.vreg_len, ctrl_state=vertex)
+
+                circuit.append(cvcoin, list(range(self.reg_len)))
+                
             circuit.barrier()
 
         return circuit
+
+
+    def counter(self, inc=1):
+        start = 0
+        end = self.N
+
+        if inc == 1:
+            ctrl_state = "1"
+        else:
+            ctrl_state = "0"
+
+        for i in range(start, end - 1):
+
+            target_qubit = i
+            mcx_len = end - i
+            num_control = mcx_len - 1
+            mcx = QuantumCircuit(mcx_len)
+            print(np.arange(i + 1, mcx_len, dtype=int))
+            mcx.mcx(np.arange(i + 1, mcx_len, dtype=int), target_qubit)
+            mcx.to_gate().control(
+                num_ctrl_qubits=num_control,
+                ctrl_state=num_control * ctrl_state)
+        return mcx
+
+
+    def simpler_flip_flop(self):
+
+        flip_flop = QuantumCircuit(self.reg_len)
+        
+        # incrementer
+        inc_x = QuantumCircuit(self.vreg_len)
+        inc_x.append(self.counter(1), range(self.N))
+        inc_x.to_gate().control(num_ctrl_qubits=2, ctrl_state="11")
+        flip_flop.append(inc_x, [0, 1, 4, 5])
+        flip_flop.barrier()
+
+         # incrementer
+        dec_x = QuantumCircuit(self.vreg_len)
+        dec_x.append(self.counter(0), range(self.N))
+        dec_x.to_gate().control(num_ctrl_qubits=2, ctrl_state="10")
+        flip_flop.append(dec_x, [0, 1, 4, 5])
+        flip_flop.barrier()
+
+         # incrementer
+        inc_y = QuantumCircuit(self.vreg_len)
+        inc_y.append(self.counter(1), range(self.M))
+        inc_y.to_gate().control(num_ctrl_qubits=2, ctrl_state="01")
+        flip_flop.append(inc_y, [2, 3, 4, 5])
+        flip_flop.barrier()
+
+         # incrementer
+        dec_y = QuantumCircuit(self.vreg_len)
+        dec_y.append(self.counter(0), range(self.M))
+        dec_y.to_gate().control(num_ctrl_qubits=2, ctrl_state="00")
+        flip_flop.append(dec_y, [2, 3, 4, 5])
+        flip_flop.barrier()
+
+        return flip_flop
+
 
 
     # def flip_flop_circuit(self):
