387d5b53d1aa99a17b71d96f7eb3265046d820f2
mohfaz <mobayen.mohammad@gmail.com>
2021-01-30 23:34:35 -0500

    Now ui prompt user to select a gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-# form something import somehting* TODO
+# TODO form something import somehting
+
 class Cell():
     FILLED_COLOR_BG = "green"
     EMPTY_COLOR_BG = "white"
@@ -75,10 +76,38 @@ class CellGrid(Canvas):
         row = int(event.y / self.cellSize)
         column = int(event.x / self.cellSize)
         return row, column
+    def putGate(self,root,str):
+        print(str) # TODO you get the gate here
+        root.destroy()
+    def ask_gate(self):
+        
+        root = Tk()
+        root.title("title")
+    
+        w = 400     # popup window width
+        h = 400     # popup window height
+    
+        sw = root.winfo_screenwidth()
+        sh = root.winfo_screenheight()
+    
+        x = (sw - w)/2
+        y = (sh - h)/2
+        root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+    
+        w = Label(root, text="Please select your desired gate", width=120, height=10)
+        w.pack()
+        hadamardGate = Button(root, text="Hadamard Gate", command=lambda : self.putGate(root,'Hadamard is selected'), width=10).pack()
+        groverGate   = Button(root, text="Grover Gate", command=lambda : self.putGate(root,'Grover is selected'), width=10).pack()
+        otherGate    = Button(root, text="other Gate", command=lambda : self.putGate(root,'other gate is selected'), width=10).pack()
+        
+        mainloop()
+    
 
     def handleMouseClick(self, event):
         row, column = self._eventCoords(event)
-        # your function that gets row and column TODO
+        
+        # TODO your function that gets row and column 
+        
         cell_list = list()
         for row_index in range(row - 1, row+2, 2):
             if row_index < 0 or row_index >= self.rowNumber :
@@ -86,23 +115,30 @@ class CellGrid(Canvas):
             cell = self.grid[row_index][column]
             cell._switch()
             cell_list.append(cell)
-          
+         
         for column_index in range(column - 1, column+2, 2):
             if column_index < 0 or column_index >= self.columnNumber :
                 continue
-            print(self.grid[0][0].fill)
+            # print(self.grid[0][0].fill)
             cell = self.grid[row][column_index]
             cell._switch()
             cell_list.append(cell)
 
-            # cell.draw()
-            # self.switched.append(cell)
-               
-        for i in cell_list :
-            i.draw()      
-            self.switched.append(i)
-        
-        #add the cell to the list of cell switched during the click
+        for element in cell_list :
+            element.draw()      
+            self.switched.append(element)
+        window = Tk()
+        window.wm_withdraw()
+        self.ask_gate()
+    
+# #message at x:200,y:200
+#         window.geometry("1x1+200+200")#remember its .geometry("WidthxHeight(+or-)X(+or-)Y")
+#         # tkMessageBox.showerror(title="error",message="Error Message",parent=window)
+
+# #centre screen message
+#         window.geometry("1x1+"+str(window.winfo_screenwidth()/2)+"+"+str(window.winfo_screenheight()/2))
+#         # tkMessageBox.showinfo(title="Greetings", message="Hello World!")
+#         #add the cell to the list of cell switched during the click
         
     
     def handleMouseMotion(self, event):
@@ -110,7 +146,7 @@ class CellGrid(Canvas):
         cell = self.grid[row][column]
 
         if cell not in self.switched:
-            #cell._switch()
+            #cell._switch() # not sure what this do so I commented it 
             cell.draw()
             self.switched.append(cell)
     def neighbors(row,column):
